865b80113c06ee09b3ff54ed641a5f5da2340e48
mwardbaranyay <65984038+mwardbaranyay@users.noreply.github.com>
2020-08-28 13:17:50 -0700

    Updated geographical box and global projection
    Updated geographical box to exclude Ellesmere Island in the northwest and include entirety of Greenland and extended map to longtiude 0

--- compare_LWvsIGBP_inset.py
+++ compare_LWvsIGBP_inset.py
@@ -1,5 +1,6 @@
 """
 Edited by Hannah Skobe
+Edited by Megan Ward-Baranyay
 
 This example code illustrates how to access and visualize a LaRC CALIPSO file 
 in Python on land above Greenland. Specifically, this code plots a comparison
@@ -100,7 +101,7 @@ cmap = colors.ListedColormap(['blue', 'green'])
 # The data is global, so render in a global projection
 m = Basemap(projection='cyl', resolution='l',
             llcrnrlat=55, urcrnrlat=90,
-            llcrnrlon=-80, urcrnrlon=-10)
+            llcrnrlon=-80, urcrnrlon=0)
 m.drawcoastlines(linewidth=0.5)
 m.drawparallels(np.arange(55,85,10), labels=[True,False,False,True])
 m.drawmeridians(np.arange(-80,-10,10), labels=[True,False,False,True])
@@ -123,34 +124,29 @@ for m2_feature in IGBP:
 m.plot(lon, lat, color='grey', linewidth=0.75)
 
 # Draw geographical box
-for i in lat:
-    if 75 <= i <= 83: 
-        m.scatter(-73, i, color='red', s=2)
-    elif 67 <= i <= 75: 
-        m.scatter(-60, i, color='red', s=2)
-    elif 58 <= i <= 67: 
-        m.scatter(-55, i, color='red', s=2)
-for j in lat:
-    if 75 <= j <= 83: 
-        m.scatter(-15, j, color='red', s=2)
-    elif 67 <= j <= 75: 
-        m.scatter(-17, j, color='red', s=2)
-    elif 58 <= j <= 67: 
-        m.scatter(-30, j, color='red', s=2)
-for i in lon:
-    if -73 <= i <= -15: 
-        m.scatter(i, 83, color='red', s=2)
-for j in lon:
-    if -73 <= j <= -60:
-        m.scatter(j, 75, color='red', s=2)
-    elif -60 <= j <=-55:
-        m.scatter(j, 67, color='red', s=2)
-    elif -55 <= j <= -30: 
-        m.scatter(j, 58, color='red', s=2)
-    elif -30 <= j <= -17:
-        m.scatter(j, 67, color='red', s=2)
-    elif -17 <= j <= -15:
-        m.scatter(j, 75, color='red', s=2)
+x1, y1 = [-60, -60], [82, 85]
+x2, y2 = [-62, -60], [82, 82]
+x3, y3 = [-64, -64], [81, 81.5]
+x4, y4 = [-68, -64], [81, 81]
+x5, y5 = [-68, -68], [80, 81]
+x6, y6 = [-70, -68], [80, 80]
+x7, y7 = [-70, -70], [79, 80]
+x8, y8 = [-73, -70], [79, 79]
+x9, y9 = [-60, -60], [67, 75]
+x10, y10 = [-55, -55], [58, 67]
+x11, y11 = [-10, -10], [75, 85]
+x12, y12 = [-17, -17], [67, 75]
+x13, y13 = [-30, -30], [58, 67]
+x14, y14 = [-60, -10], [85, 85]
+x15, y15 = [-73, -60], [75, 75]
+x16, y16 = [-60, -55], [67, 67]
+x17, y17 = [-55, -30], [58, 58]
+x18, y18 = [-30, -17], [67, 67]
+x19, y19 = [-17, -10], [75, 75]
+x20, y20 = [-73, -73], [75, 79]
+x21, y21 = [-64, -62], [81.5, 81.5]
+x22, y22 = [-62, -62], [81.5, 82]
+plt.plot(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13, x14, y14, x15, y15, x16, y16, x17, y17, x18, y18, x19, y19, x20, y20, x21, y21, x22, y22, marker = 'o', color='red', markersize=1)
         
 #-----------------------------------------------------------------------------#
 # Create lower left inset
